10f0242809e2f5c07b08d162d47c0a2662bf7331
AFuentes88 <fuentes-alejandro@outlook.com>
2022-12-26 01:43:12 -0300

    various changes, no results

--- template/my_component/frontend/src/MyComponent.tsx
+++ template/my_component/frontend/src/MyComponent.tsx
@@ -2,88 +2,62 @@ import {
   Streamlit,
   StreamlitComponentBase,
   withStreamlitConnection,
-} from "streamlit-component-lib"
-import React, { ReactNode } from "react"
+} from 'streamlit-component-lib'
+
+import * as React from 'react';
+import { useState } from 'react';
+import { Select, Checkbox, Form } from 'antd';
+import { Callbacks } from 'rc-field-form/lib/interface';
 
 interface State {
-  numClicks: number
-  isFocused: boolean
+  options: string
+  onChange: Callbacks
 }
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
-class MyComponent extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0, isFocused: false }
+const { Option } = Select;
 
-  public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
-    const name = this.props.args["name"]
-
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
-
-    // Maintain compatibility with older versions of Streamlit that don't send
-    // a theme object.
-    if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
-    }
-
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
-    // variable, and send its new value back to Streamlit, where it'll
-    // be available to the Python program.
-    return (
-      <span>
-        Hello, {name}! &nbsp;
-        <button
-          style={style}
-          onClick={this.onClicked}
-          disabled={this.props.disabled}
-          onFocus={this._onFocus}
-          onBlur={this._onBlur}
-        >
-          Click Me!
-        </button>
-      </span>
-    )
-  }
-
-  /** Click handler for our "Click Me!" button. */
-  private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-    this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
-      () => Streamlit.setComponentValue(this.state.numClicks)
-    )
-  }
-
-  /** Focus handler for our "Click Me!" button. */
-  private _onFocus = (): void => {
-    this.setState({ isFocused: true })
-  }
-
-  /** Blur handler for our "Click Me!" button. */
-  private _onBlur = (): void => {
-    this.setState({ isFocused: false })
-  }
+interface Props {
+  options: string[];
+  onChange: (selectedOptions: string[]) => void;
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
-export default withStreamlitConnection(MyComponent)
+const DropdownWidget: React.FC<Props> = ({ options, onChange }) => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const handleChange = (selectedOptions: string[]) => {
+    setSelectedOptions(selectedOptions);
+    onChange(selectedOptions);
+  };
+
+  return (
+    <Form.Item>
+      <Select
+        mode="multiple"
+        style={{ width: '100%' }}
+        placeholder="Please select"
+        onChange={handleChange}
+      >
+        {options.map((option) => (
+          <Option key={option} value={option}>
+            {option}
+          </Option>
+        ))}
+      </Select>
+      <Checkbox
+        indeterminate={selectedOptions.length > 0 && selectedOptions.length < options.length}
+        onChange={(e) => {
+          if (e.target.checked) {
+            handleChange(options);
+          } else {
+            handleChange([]);
+          }
+        }}
+        checked={selectedOptions.length === options.length}
+      >
+        Select all
+      </Checkbox>
+    </Form.Item>
+  );
+};
+
+export default DropdownWidget;
--- template/my_component/frontend/src/index.tsx
+++ template/my_component/frontend/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./MyComponent"
+import DropdownWidget from './MyComponent'
+
+const options = ['Option 1', 'Option 2', 'Option 3'];
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent />
+    <DropdownWidget options={options} onChange={(selectedOptions) => console.log(selectedOptions)} />
   </React.StrictMode>,
   document.getElementById("root")
 )
